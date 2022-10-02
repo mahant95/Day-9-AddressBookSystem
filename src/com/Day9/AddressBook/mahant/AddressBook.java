299226f111d16ac8ed2fe9ad14a6d05cb311014a
mahant95 <montyempire95@gmail.com>
2022-10-02 19:21:46 +0530

    Displayed a message For AddressBook

---
+++ src/com/Day9/AddressBook/mahant/AddressBook.java
@@ -0,0 +1,12 @@
+package com.Day9.AddressBook.mahant;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Address Book System Program");
+		
+
+	}
+
+}
