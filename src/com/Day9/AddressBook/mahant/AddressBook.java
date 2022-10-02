ac1b086fed2f399875efb1f85ab30712a88b7591
mahant95 <montyempire95@gmail.com>
2022-10-02 19:29:19 +0530

    created a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email...

--- src/com/Day9/AddressBook/mahant/AddressBook.java
+++ src/com/Day9/AddressBook/mahant/AddressBook.java
@@ -1,12 +1,22 @@
 package com.Day9.AddressBook.mahant;
+import java.util.Scanner;
 
 public class AddressBook {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome To Address Book System Program");
-		
-
-	}
-
+        System.out.println("WelCome To AddressBook Program");
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the first name");
+        String  fName = sc.next();
+        System.out.println("Enter the last Name");
+        String lName = sc.next();
+        System.out.println("Enter the city");
+        String City = sc.next();
+        System.out.println("Enter the Address");
+        String Address = sc.next();
+        System.out.println("Enter the number");
+        int Number = sc.nextInt();
+        System.out.println("Enter the mail");
+        String Email = sc.next();
+    }
 }
