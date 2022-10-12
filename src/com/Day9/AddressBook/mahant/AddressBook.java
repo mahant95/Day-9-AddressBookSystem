33c39dbcaecfe435e98097756d0fb36de95e9cbf
Mahant Angadi <63593175+mahant95@users.noreply.github.com>
2022-10-12 21:27:24 +0530

    Update AddressBook.java

--- src/com/Day9/AddressBook/mahant/AddressBook.java
+++ src/com/Day9/AddressBook/mahant/AddressBook.java
@@ -1,41 +1,74 @@
 package com.Day9.AddressBook.mahant;
-import java.util.*;
+import java.util.Scanner;
 
 public class AddressBook {
-	String Name ;
-    String Address;
-    String City;
-    String Email;
-   int Number;
-    public void AddNew(){
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter the Name");
-        Name = sc.nextLine();
-        System.out.println("Enter the address");
-        Address = sc.nextLine();
-        System.out.println("Enter the City");
-        City = sc.nextLine();
-        System.out.println("Enter the mail");
-        Email = sc.nextLine();
-        System.out.println("Enter the Number");
-        Number = sc.nextInt();
-    }
-    void deletePerson(String name) {
-        if (Name.equals(name)) {
-            Name = null;
-            Number = 0;
-            Address = null;
-            City = null;
-            Email = null;
-            System.out.println("The Data is Deleted");
-        }else{
-            System.out.println("Data Is Not Deleted ");
-        }
-    }
+	public String firstName;
+	public String lastName;
+	public String phoneNumber;
+	public String address;
+	public int pincode;
+	public String email;
 
-    public static void main(String[] args) {
-       AddressBook ads = new AddressBook();
-       ads.AddNew();
-       ads.deletePerson(ads.Name);
-    }
+	public AddressBook(String firstName, String lastName,String phoneNumber, String address
+			, int pincode, String email) {
+
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.phoneNumber=phoneNumber;
+		this.address=address;
+		this.pincode=pincode;
+		this.email=email;
+	}
+	public void newAddressBookDataEnter() {
+		System.out.println(firstName+" "+lastName+" "+phoneNumber+
+				" "+address+" "+pincode+"  "+email);
+
+	}
+	public void changePhoneNumber(String change) {
+		Scanner sc=new Scanner(System.in);
+		if (change.equals(firstName)) {
+			System.out.println("Enter The New Number");
+			phoneNumber=sc.next();
+			System.out.println(firstName+" "+lastName+" "+phoneNumber+
+					" "+address+" "+pincode+"  "+email);
+		}
+	}
+	public void Deletinfunc(String name) {
+		if (firstName.equals(name)) {
+		firstName=null;
+		lastName=null;
+		phoneNumber=null;
+		address=null;
+		pincode=0;
+		email=null;
+	System.out.println("The Data is Deleted");
+	}
+		else { System.out.println("The Data is Not Deleted");
+		}
+		}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("First Name Enter");
+		String firstName=sc.next();
+		System.out.println("Last Name Enter");
+		String lastName=sc.next();
+		System.out.println("PhoneNumber Enter");
+		String phoneNumber=sc.next();
+		System.out.println(" Address Enter");
+		String address=sc.next();
+		System.out.println(" PinCode Enter");
+		int pincode=sc.nextInt();
+		System.out.println(" Email Enter");
+		String email=sc.next();
+		AddressBook obj=new AddressBook(firstName,lastName,phoneNumber,address,pincode,email);
+		obj.newAddressBookDataEnter();
+		System.out.println("If Change The Phone Number Enter Name & Dont change Enter 0");
+		String value=sc.next();
+		obj.changePhoneNumber(value);
+		System.out.println("If Delete  The  Entery Write The Name");
+		value=sc.next();
+		obj.Deletinfunc(value);
+	}
+
 }
