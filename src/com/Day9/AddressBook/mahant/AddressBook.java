bc9bbd4e171c64c839713428b834d298954921a7
Mahant Angadi <63593175+mahant95@users.noreply.github.com>
2022-10-12 21:22:55 +0530

    Update AddressBook.java

--- src/com/Day9/AddressBook/mahant/AddressBook.java
+++ src/com/Day9/AddressBook/mahant/AddressBook.java
@@ -2,21 +2,45 @@ package com.Day9.AddressBook.mahant;
 import java.util.Scanner;
 
 public class AddressBook {
+	public String firstName;
+	public String lastName;
+	public String phoneNumber;
+	public String address;
+	public int pincode;
+	public String email;
+
+	public AddressBook(String firstName, String lastName,String phoneNumber, String address
+			, int pincode, String email) {
+
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.phoneNumber=phoneNumber;
+		this.address=address;
+		this.pincode=pincode;
+		this.email=email;
+	}
+	public void newAddressBookDataEnter() {
+		System.out.println(firstName+" "+lastName+" "+phoneNumber+
+				" "+address+" "+pincode+"  "+email);
+
+	}
 
 	public static void main(String[] args) {
-        System.out.println("WelCome To AddressBook Program");
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter the first name");
-        String  fName = sc.next();
-        System.out.println("Enter the last Name");
-        String lName = sc.next();
-        System.out.println("Enter the city");
-        String City = sc.next();
-        System.out.println("Enter the Address");
-        String Address = sc.next();
-        System.out.println("Enter the number");
-        int Number = sc.nextInt();
-        System.out.println("Enter the mail");
-        String Email = sc.next();
-    }
-}
+		Scanner sc=new Scanner(System.in);
+		System.out.println("First Name Enter");
+		String firstName=sc.next();
+		System.out.println("Last Name Enter");
+		String lastName=sc.next();
+		System.out.println("PhoneNumber Enter");
+		String phoneNumber=sc.next();
+		System.out.println(" Address Enter");
+		String address=sc.next();
+		System.out.println(" PinCode Enter");
+		int pincode=sc.nextInt();
+		System.out.println(" Email Enter");
+		String email=sc.next();
+		AddressBook addrsBook=new AddressBook(firstName,lastName,phoneNumber,address,pincode,email);
+	addrsBook.newAddressBookDataEnter();
+	}
+
+	}
